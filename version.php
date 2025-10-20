53d3b05d5e2e4d74d250f6ea58816dcd5724127b
Pavel <Pavel.Pasechnik@gmail.com>
2025-10-20 18:41:03 +0300

    First commit

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_autonumber';   // Unique plugin identifier
+$plugin->version   = 2025102000;           // YearMonthDayVersion
+$plugin->requires  = 2022041900;           // Minimum Moodle version (4.0+)
+$plugin->release   = '1.0';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->cron      = 0;
